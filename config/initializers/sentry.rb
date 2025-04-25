de6ecb0b732e38e8f4e338ed9dd4bac60f118737
Eric Roos <ericroos@hey.com>
2025-04-25 10:55:53 -0500

    add sentry tracing

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,4 +6,12 @@ Sentry.init do |config|
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
   config.send_default_pii = true
+
+  config.traces_sample_rate = .2
+  # or control sampling dynamically
+  config.traces_sampler = lambda do |sampling_context|
+    # sampling_context[:transaction_context] contains the information about the transaction
+    # sampling_context[:parent_sampled] contains the transaction's parent's sample decision
+    true # return value can be a boolean or a float between 0.0 and 1.0
+  end
 end
